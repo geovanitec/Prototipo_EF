070814b362c925949d4cecc1fa23912fe2bb776a
geovanitec <geovanifermen@gmail.com>
2021-11-23 11:29:45 -0600

    RamaContendor9959-18-15407

---
+++ PrototipoEF/CapaVistaMoviemiento/frmMDI.Designer.cs
@@ -0,0 +1,170 @@
+﻿
+namespace CapaVistaMoviemiento
+{
+    partial class frmMDI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.aBRIRToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cATALOGOSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pROCESOSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.iNFORMESToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hERRAMIENTASToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aYUDASToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mANTENIMIENTOCUENTACONTABLEToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mANTENIMIENTOCUENTABANCARIAToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aYUDASToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.rEPORTESToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mOVIMIENTOBANCARIOToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(18, 18);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aBRIRToolStripMenuItem,
+            this.cATALOGOSToolStripMenuItem,
+            this.pROCESOSToolStripMenuItem,
+            this.iNFORMESToolStripMenuItem,
+            this.hERRAMIENTASToolStripMenuItem,
+            this.aYUDASToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(874, 27);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // aBRIRToolStripMenuItem
+            // 
+            this.aBRIRToolStripMenuItem.Name = "aBRIRToolStripMenuItem";
+            this.aBRIRToolStripMenuItem.Size = new System.Drawing.Size(58, 23);
+            this.aBRIRToolStripMenuItem.Text = "ABRIR";
+            // 
+            // cATALOGOSToolStripMenuItem
+            // 
+            this.cATALOGOSToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mANTENIMIENTOCUENTACONTABLEToolStripMenuItem,
+            this.mANTENIMIENTOCUENTABANCARIAToolStripMenuItem});
+            this.cATALOGOSToolStripMenuItem.Name = "cATALOGOSToolStripMenuItem";
+            this.cATALOGOSToolStripMenuItem.Size = new System.Drawing.Size(99, 23);
+            this.cATALOGOSToolStripMenuItem.Text = "CATALOGOS";
+            // 
+            // pROCESOSToolStripMenuItem
+            // 
+            this.pROCESOSToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mOVIMIENTOBANCARIOToolStripMenuItem});
+            this.pROCESOSToolStripMenuItem.Name = "pROCESOSToolStripMenuItem";
+            this.pROCESOSToolStripMenuItem.Size = new System.Drawing.Size(89, 23);
+            this.pROCESOSToolStripMenuItem.Text = "PROCESOS";
+            // 
+            // iNFORMESToolStripMenuItem
+            // 
+            this.iNFORMESToolStripMenuItem.Name = "iNFORMESToolStripMenuItem";
+            this.iNFORMESToolStripMenuItem.Size = new System.Drawing.Size(88, 23);
+            this.iNFORMESToolStripMenuItem.Text = "INFORMES";
+            // 
+            // hERRAMIENTASToolStripMenuItem
+            // 
+            this.hERRAMIENTASToolStripMenuItem.Name = "hERRAMIENTASToolStripMenuItem";
+            this.hERRAMIENTASToolStripMenuItem.Size = new System.Drawing.Size(119, 23);
+            this.hERRAMIENTASToolStripMenuItem.Text = "HERRAMIENTAS";
+            // 
+            // aYUDASToolStripMenuItem
+            // 
+            this.aYUDASToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aYUDASToolStripMenuItem1,
+            this.rEPORTESToolStripMenuItem});
+            this.aYUDASToolStripMenuItem.Name = "aYUDASToolStripMenuItem";
+            this.aYUDASToolStripMenuItem.Size = new System.Drawing.Size(73, 23);
+            this.aYUDASToolStripMenuItem.Text = "AYUDAS";
+            // 
+            // mANTENIMIENTOCUENTACONTABLEToolStripMenuItem
+            // 
+            this.mANTENIMIENTOCUENTACONTABLEToolStripMenuItem.Name = "mANTENIMIENTOCUENTACONTABLEToolStripMenuItem";
+            this.mANTENIMIENTOCUENTACONTABLEToolStripMenuItem.Size = new System.Drawing.Size(321, 24);
+            this.mANTENIMIENTOCUENTACONTABLEToolStripMenuItem.Text = "MANTENIMIENTO CUENTA CONTABLE";
+            // 
+            // mANTENIMIENTOCUENTABANCARIAToolStripMenuItem
+            // 
+            this.mANTENIMIENTOCUENTABANCARIAToolStripMenuItem.Name = "mANTENIMIENTOCUENTABANCARIAToolStripMenuItem";
+            this.mANTENIMIENTOCUENTABANCARIAToolStripMenuItem.Size = new System.Drawing.Size(321, 24);
+            this.mANTENIMIENTOCUENTABANCARIAToolStripMenuItem.Text = "MANTENIMIENTO CUENTA BANCARIA";
+            // 
+            // aYUDASToolStripMenuItem1
+            // 
+            this.aYUDASToolStripMenuItem1.Name = "aYUDASToolStripMenuItem1";
+            this.aYUDASToolStripMenuItem1.Size = new System.Drawing.Size(198, 24);
+            this.aYUDASToolStripMenuItem1.Text = "AYUDAS";
+            // 
+            // rEPORTESToolStripMenuItem
+            // 
+            this.rEPORTESToolStripMenuItem.Name = "rEPORTESToolStripMenuItem";
+            this.rEPORTESToolStripMenuItem.Size = new System.Drawing.Size(198, 24);
+            this.rEPORTESToolStripMenuItem.Text = "REPORTES";
+            // 
+            // mOVIMIENTOBANCARIOToolStripMenuItem
+            // 
+            this.mOVIMIENTOBANCARIOToolStripMenuItem.Name = "mOVIMIENTOBANCARIOToolStripMenuItem";
+            this.mOVIMIENTOBANCARIOToolStripMenuItem.Size = new System.Drawing.Size(248, 24);
+            this.mOVIMIENTOBANCARIOToolStripMenuItem.Text = "MOVIMIENTO BANCARIO ";
+            // 
+            // frmMDI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(874, 505);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmMDI";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmMDI";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem aBRIRToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cATALOGOSToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mANTENIMIENTOCUENTACONTABLEToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mANTENIMIENTOCUENTABANCARIAToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pROCESOSToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem iNFORMESToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hERRAMIENTASToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aYUDASToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mOVIMIENTOBANCARIOToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aYUDASToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem rEPORTESToolStripMenuItem;
+    }
+}
